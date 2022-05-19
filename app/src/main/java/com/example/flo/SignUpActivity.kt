76c1184c8f1d0b1eb1ed0b92a9b450ae0cdf2ea3
jeeminimini <jiminn821@naver.com>
2022-05-19 23:25:52 +0900

    error 9th assignment

--- app/src/main/java/com/example/flo/SignUpActivity.kt
+++ app/src/main/java/com/example/flo/SignUpActivity.kt
@@ -2,9 +2,14 @@ package com.example.flo
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySignupBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
@@ -16,20 +21,44 @@ class SignUpActivity : AppCompatActivity() {
 
         binding.signUpSignUpBtn.setOnClickListener {
             signUp()
-            finish() //finish를 통해서 로그인 화면으로 이동을 하면서 회원가입 엑티비티가 꺼진다.
+            //finish() //finish를 통해서 로그인 화면으로 이동을 하면서 회원가입 엑티비티가 꺼진다.
         }
     }
 
     private fun getUser(): User{
         val email : String = binding.signUpIdEt.text.toString() + "@" + binding.signUpEmailEt.text.toString()
         val pwd: String = binding.signUpPasswordEt.text.toString()
+        val name: String = binding.signUpNameEt.text.toString()
 
-        return User(email, pwd)
+        return User(email, pwd, name)
     }
 
-    private fun signUp(){
+//    private fun signUp(){ //RoomDB연결
+//        if(binding.signUpIdEt.text.toString().isEmpty() || binding.signUpEmailEt.text.toString().isEmpty()){
+//            Toast.makeText(this,"이메일 형식이 잘못되었습니다.",Toast.LENGTH_SHORT).show()
+//            return //함수 끝나도록.
+//        }
+//        if(binding.signUpPasswordEt.text.toString() != binding.signUpPasswordConfirmEt.text.toString()){
+//            Toast.makeText(this,"비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show()
+//            return //함수 끝나도록.
+//        }
+//
+//        //정보를 DB에 저장하는 방법
+//        val userDB = SongDatabase.getInstance(this)!!
+//        userDB.userDao().insert(getUser()) //getUser()는 유저가 입력한 정보이다
+//
+//        val user = userDB.userDao().getUsers() //현재 테이블에 저장되어있는 정보 가져오기
+//        Log.d("SIGNUPACT",user.toString())
+//
+//    }
+
+    private fun signUp(){ //API연결
         if(binding.signUpIdEt.text.toString().isEmpty() || binding.signUpEmailEt.text.toString().isEmpty()){
             Toast.makeText(this,"이메일 형식이 잘못되었습니다.",Toast.LENGTH_SHORT).show()
+            return //함수 끝나도록.
+        }
+        if(binding.signUpNameEt.text.toString().isEmpty()){
+            Toast.makeText(this,"이름 형식이 잘못되었습니다.",Toast.LENGTH_SHORT).show()
             return //함수 끝나도록.
         }
         if(binding.signUpPasswordEt.text.toString() != binding.signUpPasswordConfirmEt.text.toString()){
@@ -37,12 +66,26 @@ class SignUpActivity : AppCompatActivity() {
             return //함수 끝나도록.
         }
 
-        //정보를 DB에 저장하는 방법
-        val userDB = SongDatabase.getInstance(this)!!
-        userDB.userDao().insert(getUser()) //getUser()는 유저가 입력한 정보이다
+        val authService = getRetrofit().create(AuthRetrofitInterface::class.java)
+        authService.signUp(getUser()).enqueue(object: Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("SIGNUP/SUCCESS",response.toString())
+                val resp: AuthResponse = response.body()!!
+                when(resp.code){
+                    1000 -> finish()
+                    2016, 2018 ->{
+                        binding.signUpEmailErrorTv.visibility = View.VISIBLE
+                        binding.signUpEmailErrorTv.text = resp.message
 
-        val user = userDB.userDao().getUsers() //현재 테이블에 저장되어있는 정보 가져오기
-        Log.d("SIGNUPACT",user.toString())
+                    }
+                }
+            }
 
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("SIGNUP/FAILURE",t.message.toString())
+            }
+
+        })
+        Log.d("SIGNUP","HELLO")
     }
 }
